1034e8693d0d24ab09704e3ea3aafc044bf24ca8
getsimonz <67111720+getsimonz@users.noreply.github.com>
2021-11-10 19:16:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,38 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "tf_security_group" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
-
 # Create AWS ec2 instance
 resource "aws_instance" "tf_Instance" {
   ami           = var.ami_id
