b195fcd33810bddf73acbf039004976366d72193
getsimonz <67111720+getsimonz@users.noreply.github.com>
2021-11-10 19:17:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "security_group" {
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "ec2-instance"
+  default     = "ec2-instance-2"
 }
 
 variable "ami_id" {
